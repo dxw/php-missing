91dd928acdf7a4868ec230dededc0eedd74e196c
Tom Adams <tom@dxw.com>
2012-10-15 16:12:59 -0400

    Remove get_r => print_r(..., true). Add array_flatten.

--- lib/stdlib.php
+++ lib/stdlib.php
@@ -8,10 +8,10 @@ function endswith($haystack, $needle) {
   return substr($haystack, -strlen($needle)) === $needle;
 }
 
-function get_r($x) {
-  ob_start();
-  print_r($x);
-  return ob_get_clean();
+function array_flatten($array) {
+  $return = array();
+  array_walk_recursive($array, function($a) use (&$return) { $return[] = $a; });
+  return $return;
 }
 
 function parse_strptime($a) {
