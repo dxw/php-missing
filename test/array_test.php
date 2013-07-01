3bc888f5ac9e409b2ca84bc0690f73c0902dc42f
Tom Adams <tom@dxw.com>
2013-07-01 10:18:09 -0400

    Add a test for flatten

--- test/array_test.php
+++ test/array_test.php
@@ -2,7 +2,13 @@
 
 class ArrayTest extends PHPUnit_Framework_TestCase {
   function testFlatten() {
-    // Missing\_Array\flatten()
-    $this->markTestIncomplete();
+    $this->assertEquals(
+      array('a', 'b', 'c', 'd'),
+      Missing\_Array\flatten(array(
+        array('a','b'),
+        'c',
+        array(array(array('d'))),
+      ))
+    );
   }
 }
